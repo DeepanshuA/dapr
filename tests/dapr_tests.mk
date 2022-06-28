e9130c671d1e6fa2213a7c81e650fca70fa17680
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2022-06-28 19:11:59 +0530

    running specific apps
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -13,10 +13,7 @@
 
 # E2E test app list
 # e.g. E2E_TEST_APPS=hellodapr state service_invocation
-E2E_TEST_APPS= \
-secretapp \
-metadata \
-component_health \
+E2E_TEST_APPS= secretapp metadata component_health
 
 # PERFORMANCE test app list
 PERF_TEST_APPS=
