ba6003bc4de869972eba627239505b053d9498d4
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2022-06-28 21:15:43 +0530

    renamed component health packages
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- tests/e2e/componenthealth/componenthealth_test.go
+++ tests/e2e/componenthealth/componenthealth_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package component_health_e2e
+package componenthealth_e2e
 
 import (
 	"encoding/json"
