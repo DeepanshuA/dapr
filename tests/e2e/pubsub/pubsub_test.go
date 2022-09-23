59e91b83fcdd8fa8e84e54b3140521dc3c8fa977
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2022-09-23 19:12:25 +0530

    Fix stringArray
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- tests/e2e/pubsub/pubsub_test.go
+++ tests/e2e/pubsub/pubsub_test.go
@@ -234,6 +234,9 @@ func testPublishForBulkSubscribe(t *testing.T, publisherExternalURL string, prot
 	// require.NoError(t, err)
 	// offset += numberOfMessagesToPublish + 1
 
+	sentTopicBulkSubMessages = make([]string, 0)
+	sentTopicRawBulkSubMessages = make([]string, 0)
+
 	return receivedMessagesResponse{
 		ReceivedByTopicBulkSub:    sentTopicBulkSubMessages,
 		ReceivedByTopicRawBulkSub: sentTopicRawBulkSubMessages,
