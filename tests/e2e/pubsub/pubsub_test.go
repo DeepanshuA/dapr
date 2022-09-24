48a7228eef561ec61ac83b0896e7abdd54f4525b
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2022-09-24 13:35:53 +0530

    Fix unmarshalling
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- tests/e2e/pubsub/pubsub_test.go
+++ tests/e2e/pubsub/pubsub_test.go
@@ -419,7 +419,7 @@ func validateMessagesReceivedBySubscriber(t *testing.T, publisherExternalURL str
 		}
 
 		log.Printf(
-			"subscriber received %d/%d messages on pubsub-a-topic, %d/%d on pubsub-b-topic and %d/%d on pubsub-c-topic, %d/%d on pubsub-raw-topic and %d/%d on dead letter topic and and %d/%d on bulk sub topic and %d/%d on bulk raw sub topic and %d/%d on bulk ce sub topic ",
+			"subscriber received %d/%d messages on pubsub-a-topic, %d/%d on pubsub-b-topic and %d/%d on pubsub-c-topic, %d/%d on pubsub-raw-topic and %d/%d on dead letter topic",
 			len(appResp.ReceivedByTopicA), len(sentMessages.ReceivedByTopicA),
 			len(appResp.ReceivedByTopicB), len(sentMessages.ReceivedByTopicB),
 			len(appResp.ReceivedByTopicC), len(sentMessages.ReceivedByTopicC),
@@ -497,7 +497,7 @@ func validateMessagesReceivedByBulkSubscriber(t *testing.T, publisherExternalURL
 		}
 
 		log.Printf(
-			"subscriber received %d/%d on bulk sub topic and %d/%d on bulk raw sub topic and %d/%d on bulk ce sub topic ",
+			"subscriber received %d/%d on bulk ce sub topic ",
 			len(appResp.ReceivedByTopicCEBulkSub), len(sentMessages.ReceivedByTopicCEBulkSub),
 		)
 
